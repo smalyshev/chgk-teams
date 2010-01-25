3c5e8493fca85a963b79b52a5f3f71db652f4909
Stanislav Malyshev <smalyshev@gmail.com>
2010-01-25 02:17:50 +0000

    always CC zavkadr

--- library/Reg2/Mail.php
+++ library/Reg2/Mail.php
@@ -1,7 +1,13 @@
 <?php
 class Reg2_Mail
 {
+    /**
+     * @var Zend_Mail
+     */
     protected $_mailer;
+    /**
+     * @var Zend_View
+     */
     protected $_view;
     protected $_template;
     function __construct ($template)
@@ -10,6 +16,8 @@ class Reg2_Mail
         $this->_view = new Zend_View();
         $this->_template = $template;
         $this->_view->setScriptPath(APPLICATION_PATH. '/views/scripts/email');
+        $config = Bootstrap::get('config');
+        $this->_mailer->addCc($config["mail"]["kadavr"]);
     }
     function getMailer ()
     {
@@ -21,6 +29,7 @@ class Reg2_Mail
     }
     function send ()
     {
+    	Zend_Registry::get('log')->info("Sending email {$this->_template}");
         $this->_mailer->setBodyText($this->_view->render($this->_template . ".phtml"), 'UTF-8');
         $this->_mailer->send();
     }
