1b1997283c9aaf9aa2193a389474281ace8a20fc
Stanislav Malyshev <smalyshev@gmail.com>
2010-02-02 18:57:51 +0000

    import env

--- public/index.php
+++ public/index.php
@@ -5,10 +5,11 @@ defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
 // Define application environment
-define('APPLICATION_ENV', 'development');
+define('APPLICATION_ENV', isset($_SERVER['APPLICATION_ENV'])?$_SERVER['APPLICATION_ENV']:'development');
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
+    isset($_SERVER['ZF_PATH'])?$_SERVER['ZF_PATH']:".",
     realpath(APPLICATION_PATH . '/../library'),
     get_include_path(),
 )));
