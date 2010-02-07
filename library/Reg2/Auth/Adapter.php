e0eaa446c859d7b647cdedfdcbaf43e855d871b4
Stanislav Malyshev <smalyshev@gmail.com>
2010-02-07 10:05:33 +0000

    login log

--- library/Reg2/Auth/Adapter.php
+++ library/Reg2/Auth/Adapter.php
@@ -44,11 +44,11 @@ class Reg2_Auth_Adapter implements Zend_Auth_Adapter_Interface
             );
         /** @var Zend_Config */
 		$config = Zend_Registry::get('config');
-		
 		if(isset($config['admin']['pass']) &&
 			 $this->_data['code'] == $config['admin']['pass'] && $this->_data['email'] == 'admin') {
         	$result['code'] = Zend_Auth_Result::SUCCESS;
         	$result['identity'] = new Reg2_Model_Identity('admin');
+            Zend_Registry::get('log')->info('Admin login from '.$_SERVER['REMOTE_ADDR']);		
         	return new Zend_Auth_Result($result['code'], $result['identity'], $result['messages']);
         }
         
@@ -58,9 +58,11 @@ class Reg2_Auth_Adapter implements Zend_Auth_Adapter_Interface
         	$result['code'] = Zend_Auth_Result::SUCCESS;
         	$user = $this->_dbauth->getResultRowObject();
         	$result['identity'] = new Reg2_Model_Identity($user->role, $user);
+            Zend_Registry::get('log')->info(sprintf("Login with '%s' as '%s'(%d) from %s", $this->_data['email'], $user->role, $user->tid, $_SERVER['REMOTE_ADDR']));		
         } else {
         	$result['code'] = $dbresult->getCode();
         	$result['messages'] = $dbresult->getMessages();
+            Zend_Registry::get('log')->info(sprintf("Login attempt: %d from %s", $dbresult->getCode(), $_SERVER['REMOTE_ADDR']));		
         }
         
         return new Zend_Auth_Result($result['code'], $result['identity'], $result['messages']);
