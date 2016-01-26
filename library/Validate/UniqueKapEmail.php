d55be170074f2da72f762375c8273625e95e827e
Stanislav Malyshev <smalyshev@gmail.com>
2016-01-26 00:27:24 -0800

    add debug

--- library/Validate/UniqueKapEmail.php
+++ library/Validate/UniqueKapEmail.php
@@ -4,30 +4,30 @@ class Reg2_Validate_UniqueKapEmail extends Zend_Validate_Abstract
 {
     protected $_tid; /* team id */
     protected $_id; /* team id */
-    
-    public $team;
-    
-    const MAIL_ALREADY = 'player-already';   
 
-    protected $_messageTemplates = array(   
-        self::MAIL_ALREADY => "Мейл %value% уже зарегистрирован капитаном команды %team%",   
-    );   
+    public $team;
+
+    const MAIL_ALREADY = 'player-already';
+
+    protected $_messageTemplates = array(
+        self::MAIL_ALREADY => "Мейл %value% уже зарегистрирован капитаном команды %team%",
+    );
     protected $_messageVariables = array(
         'team' => 'team',
-    );	
+    );
     public function __construct($id, $tid)
-    {   
+    {
         $this->_tid = $tid;
         $this->_id = $id;
-    }   
+    }
 
     public function isValid($value, $context = null)
-    {   
+    {
         $this->_setValue($value);
         if($this->_id > 0) {
             return true;
         }
-	Zend_Registry::get('log')->info("Unique kap: $value {$this->_tid}.");
+		Zend_Registry::get('log')->info("Unique kap: $value {$this->_tid}.");
     	if($value) {
     		$user = Reg2_Model_Data::getModel()->findUserByEmail($value);
     		if($user && $user->tid != $this->_tid) {
@@ -35,12 +35,13 @@ class Reg2_Validate_UniqueKapEmail extends Zend_Validate_Abstract
     			if(!empty($context["pold0"]) || !empty($context['pid0']) && $context['name'] == $this->team) {
     				return true;
     			}
+    			Zend_Registry::get('log')->info("Unique kap fail: $value {$this->_tid} ".var_export($context, true));
 
                	$this->_error(self::MAIL_ALREADY);
-       	        return false;   
-    		}	
+       	        return false;
+    		}
     	}
-        return true;   
-    }   
+        return true;
+    }
 }
-	
+
