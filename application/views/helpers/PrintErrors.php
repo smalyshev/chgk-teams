5d7ce0fb50cd532a60bd4ade7aaf60b8a6a967e7
Stas Malyshev <smalyshev@gmail.com>
2025-09-03 19:02:34 -0600

    One more fix

--- application/views/helpers/PrintErrors.php
+++ application/views/helpers/PrintErrors.php
@@ -1,40 +1,43 @@
 <?php
+
 class Zend_View_Helper_PrintErrors
 {
-	
-	/**
-	 * @var Zend_View_Interface 
-	 */
-	public $view;
-	
-	/**
-	 *  
-	 */
-	public function printErrors($data, $form) 
-	{
-		foreach($data as $key => $value) {
-			list($errno, $errmsg) = each($value);
-			if(is_array($errmsg)) {
-				$this->printErrors($value);
-			} else {
-				$element = $form->getElement($key);
-				if($element) {
-					$label = $element->getLabel();
-					if($label) {
-						$key = $label;
-					}
-				}
-				echo "$key: $errmsg<br>";
-			}
-		}
-	}
-	
-	/**
-	 * Sets the view field 
-	 * @param $view Zend_View_Interface
-	 */
-	public function setView(Zend_View_Interface $view) {
-		$this->view = $view;
-	}
+
+    /**
+     * @var Zend_View_Interface
+     */
+    public $view;
+
+    /**
+     *
+     */
+    public function printErrors($data, $form)
+    {
+        foreach ($data as $key => $value) {
+            $errno = key($value);
+            $errmsg = $value[$errno];
+            if (is_array($errmsg)) {
+                $this->printErrors($value);
+            } else {
+                $element = $form->getElement($key);
+                if ($element) {
+                    $label = $element->getLabel();
+                    if ($label) {
+                        $key = $label;
+                    }
+                }
+                echo "$key: $errmsg<br>";
+            }
+        }
+    }
+
+    /**
+     * Sets the view field
+     * @param $view Zend_View_Interface
+     */
+    public function setView(Zend_View_Interface $view)
+    {
+        $this->view = $view;
+    }
 }
 
